d34daab0aa78a9d3593795bc3eb8c94bcb7775bc
Mohamed Abdelhamid <mohamedabdelhamid3030@gmail.com>
2022-10-25 12:10:56 +0200

    init

---
+++ src/Point.java
@@ -0,0 +1,32 @@
+import java.awt.geom.Point2D;
+public class Point extends Point2D{
+
+    private double x, y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public double getX() {
+        return this.x;
+    }
+
+    @Override
+    public double getY() {
+        return this.y;
+    }
+
+    @Override
+    public void setLocation(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static void main(String[] args) {
+        Point p = new Point(0,0);
+        Point p1 = new Point(1,1);
+        System.out.println(p.distance(p1));
+    }
+}
